c6500debcca131cfa29f474c4ecd9630123d764b
Aniket Chakraborty <aniket2583@gmail.com>
2020-04-27 12:21:38 +0530

    First page layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  title: ,
-  home: ,
+  home: Home(),
+  routes: {
+    '/': (context) => Home(),
+  },
 ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Who are you ?"),
+      ),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: RaisedButton.icon(
+                  icon: Icon(Icons.person),
+                  onPressed: (){},
+                  label: Text("Customer"),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: RaisedButton.icon(
+                  icon: Icon(Icons.shopping_cart),
+                  onPressed: (){},
+                  label: Text("Merchant"),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
