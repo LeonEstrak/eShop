d4e74682e8a002cd1af630d2a43018205554d9b5
LeonEstrak <aniket2583@gmail.com>
2020-05-07 23:02:50 +0530

    Registration with email and password set up. Error shows correctly on failure of registration.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,46 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopwork/Authentication.dart';
+import 'package:shopwork/home.dart';
 import 'package:shopwork/loginPage.dart';
 import 'package:shopwork/registrationPage.dart';
 
-void main() => runApp(MaterialApp(
-  initialRoute: '/LoginPage',
-  routes: {
-    '/LoginPage': (context) => LoginPage(),
-    '/RegistrationPage': (context) => RegistrationPage()
-  },
-));
+void main() => runApp(Application());
+
+class Application extends StatelessWidget {
+  const Application({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+  return StreamProvider.value(
+      value: AuthenticationServices().user ,
+      child: MaterialApp(
+            initialRoute: '/',
+            routes: {
+              '/': (context) => CheckAuthentication(),
+              '/LoginPage': (context) => LoginPage(),
+              '/RegistrationPage': (context) => RegistrationPage()
+            },
+          ),
+        );
+  }
+}
+
+class CheckAuthentication extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final FirebaseUser user = Provider.of<FirebaseUser>(context);
+    //Using the stream property of Firebase, we check whether a user is logged in or not.
+    //if logged in, redirect to Home Screen else redirect to LoginPage()
+    if (user != null)
+      return Home();
+    else
+      return LoginPage();
+  }
+}
 
 //
 //class Home extends StatelessWidget {
--- lib/registrationPage.dart
+++ lib/registrationPage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shopwork/Authentication.dart';
+
+//TODO:Create a proper Registration Form with option including whether a merchant or a customer
 
 class RegistrationPage extends StatefulWidget {
   @override
@@ -6,58 +9,120 @@ class RegistrationPage extends StatefulWidget {
 }
 
 class _RegistrationPageState extends State<RegistrationPage> {
+  String email;
+
+  String password;
+
   String confirmedPassword;
+
+  String errorMessage=' ';
+
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Registration')),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              obscureText: true,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("$errorMessage"),
+              SizedBox(height: 15),
+              TextFormField(
+                //TODO:instead of isEmpty, check for validity of email [use Regular Exp.] [email.contains(new RegExp(r'...'))]
+              validator: (email) => email.isEmpty ? "Enter your e-mail" : null,
               decoration: InputDecoration(
-                  labelText: "Confirm Password", border: OutlineInputBorder()),
-              onChanged: (String confirmedPass){
-                this.confirmedPassword = confirmedPass;
+                  border: OutlineInputBorder(), labelText: "email"),
+              onChanged: (String email) {
+                this.email = email;
+                //print("email: $email");
               },
             ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          SizedBox(
-            height: 50,
-            //Buttons ROW
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
-                    child: RaisedButton(
-                      child: Text("Back"),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                  ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                validator: (password)=> password.length <= 4?"Password too small":null,
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: "Password"),
+                onChanged: (String password) {
+                  this.password = password;
+                  //print("Password: $password");
+                }),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                validator: (confirmedPassword)=>confirmedPassword!=password?"Passwords do not match":null,
+                obscureText: true,
+                decoration: InputDecoration(
+                    labelText: "Confirm Password",
+                    border: OutlineInputBorder()
                 ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
-                    child: RaisedButton(
-                      child: Text("Register"),
-                      onPressed: () {},
+                onChanged: (String confirmedPass){
+                  this.confirmedPassword = confirmedPass;
+                },
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                height: 50,
+                //Buttons ROW
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+                        child: RaisedButton(
+                          child: Text("Back"),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ),
                     ),
-                  ),
-                )
-              ],
-            ),
-          )
-        ],
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+                        child: RaisedButton(
+                          child: Text("Register"),
+                          onPressed: () async{
+                            if(_formKey.currentState.validate()){
+                              dynamic data = await AuthenticationServices().registerWithEmailAndPassword(email, password);
+                              bool userExist = data[0];
+                              dynamic userData = data[1];
+                              if( userExist== true) {
+                                print(userData);
+                                print("Registered");
+                                setState(() {
+                                  errorMessage = " ";
+                                });
+                                Navigator.popUntil(context, ModalRoute.withName('/'));
+                              }else{
+                                print("Error");
+                                setState(() {
+                                  print(userData);
+                                  errorMessage = userData.toString().split(new RegExp(r','))[1];
+                                });
+                              }
+                            }
+                          },
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
