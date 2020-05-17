f9ee6d5122c60bce174d8a28e280ca6613750bc3
LeonEstrak <aniket2583@gmail.com>
2020-05-18 01:51:39 +0530

    Local Image Assets loaded.

--- lib/home/merchantPages/MerchantPageProfile.dart
+++ lib/home/merchantPages/MerchantPageProfile.dart
@@ -9,20 +9,37 @@ class MerchantPageProfile extends StatefulWidget {
 class _MerchantPageProfileState extends State<MerchantPageProfile> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("Profile"),
-            FlatButton.icon(
-                onPressed: () {
-                  AuthenticationServices.signOut();
-                },
-                icon: Icon(Icons.person_outline),
-                label: Text("Logout"))
-          ],
-        ),
+    return SafeArea(
+      child: Column(
+//      mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(height: 20,),
+          Text(
+            "Profile",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 25,),
+          Container(
+            height: 50,
+            width: 50,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25),
+              image: DecorationImage(
+                image: AssetImage("lib/shared/One piece.png"),
+                fit: BoxFit.cover
+              )
+            ),
+          ),
+          FlatButton.icon(
+              onPressed: () {
+                AuthenticationServices.signOut();
+              },
+              icon: Icon(Icons.person_outline),
+              label: Text("Logout"))
+        ],
       ),
     );
   }
