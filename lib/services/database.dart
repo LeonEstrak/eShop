71f824fa884c0b1ee48d2236c0ec043fb0bac72e
LeonEstrak <aniket2583@gmail.com>
2021-02-11 10:50:28 +0530

    Firebase Storage initialized. Profile image uploaded to Firebase Storage. Image Picker used.

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,11 +1,36 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
 
 class DatabaseServices{
 
   final String uid;
   DatabaseServices({this.uid});
 
-  final CollectionReference databaseInstance = Firestore.instance.collection("data");
+  static final CollectionReference databaseInstance = Firestore.instance.collection("data");
+
+  static final FirebaseStorage _firebaseStorage = FirebaseStorage(storageBucket: "gs://shop-work-12bc7.appspot.com/");
+
+  StorageUploadTask _uploadTask;
+
+  void uploadImage({BuildContext context, File image}) async {
+    String filePath = "images/ProfilePhoto/$uid";
+    _uploadTask = _firebaseStorage.ref().child(filePath).putFile(image);
+    print(_uploadTask);
+  }
+
+  Future downloadImage()async{
+    String filePath = "images/ProfilePhoto/$uid";
+    try{
+
+      String result= await _firebaseStorage.ref().child(filePath).getDownloadURL();
+      return [result,true];
+    }catch(e){
+      return ["invalid File Path",false];
+    }
+  }
 
   Future registerUserData({String typeOfUser,String firstName, String lastName, String address, String mobile}) async{
     return await databaseInstance.document(uid).setData({
