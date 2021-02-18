d48a5032d434350d5e952a11508e70356fab509d
SAHASRABDI <sahasrabdi.bhattacherya18@st.niituniversity.in>
2021-02-18 17:21:00 +0530

    Added Confirmation before deleting an item

--- lib/home/merchantPages/ItemInfoCard.dart
+++ lib/home/merchantPages/ItemInfoCard.dart
@@ -34,86 +34,114 @@ class _ItemInfoCardState extends State<ItemInfoCard> {
     dynamic result = DatabaseServices(uid: user.uid).downloadItemPhoto(name);
 
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 25,
-              ),
-              Text(
-                "Item Info",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 25,
                 ),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Container(
-                child: FutureBuilder(
-                  future: result,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      if (snapshot.data[0])
-                        return Image.network(snapshot.data[1]);
-                      else
-                        return Image.asset(
-                          "lib/shared/addimage.png",
-                        );
-                    } else if (snapshot.hasError) {
-                      return Text(snapshot.error.toString());
-                    }
-                    return CircularProgressIndicator();
-                  },
+                Text(
+                  "Item Info",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-                height: 150,
-                width: 150,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.grey.withOpacity(0.2),
-                          spreadRadius: 3.0,
-                          blurRadius: 5.0)
-                    ]),
-              ),
-              SizedBox(height: 30),
-              Text("Item Name: $name"),
-              SizedBox(height: 15),
-              Text("Item Price: Rs.$price"),
-              SizedBox(height: 15),
-              Text("Item Quantity: $qty units"),
-              SizedBox(height: 15),
-              SizedBox(height: 30),
-              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-                FlatButton.icon(
-                    onPressed: () {},
-                    icon: Icon(Icons.edit),
-                    label: Text("Edit")),
-                FlatButton.icon(
-                    onPressed: () {
-                      ///TODO: Ask for confirmation before deletion of item
-                      DatabaseServices(uid: user.uid).deleteItem(
-                          itemName: name, itemPrice: price, itemQty: qty);
-                      Navigator.pop(context);
+                SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  child: FutureBuilder(
+                    future: result,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        if (snapshot.data[0])
+                          return Image.network(snapshot.data[1]);
+                        else
+                          return Image.asset(
+                            "lib/shared/addimage.png",
+                          );
+                      } else if (snapshot.hasError) {
+                        return Text(snapshot.error.toString());
+                      }
+                      return CircularProgressIndicator();
                     },
-                    icon: Icon(Icons.delete),
-                    label: Text("Delete"))
-              ]),
-              SizedBox(height: 30),
-              FlatButton.icon(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  icon: Icon(Icons.arrow_back),
-                  label: Text("Back"))
-            ],
+                  ),
+                  height: 150,
+                  width: 150,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey.withOpacity(0.2),
+                            spreadRadius: 3.0,
+                            blurRadius: 5.0)
+                      ]),
+                ),
+                SizedBox(height: 30),
+                Text("Item Name: $name"),
+                SizedBox(height: 15),
+                Text("Item Price: Rs.$price"),
+                SizedBox(height: 15),
+                Text("Item Quantity: $qty units"),
+                SizedBox(height: 15),
+                SizedBox(height: 30),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      FlatButton.icon(
+                          onPressed: () {},
+                          icon: Icon(Icons.edit),
+                          label: Text("Edit")),
+                      FlatButton.icon(
+                          onPressed: () {
+                            showDialog(
+                                context: context,
+                                builder: (context) => AlertDialog(
+                                      title: Text("Delete"),
+                                      actions: [
+                                        FlatButton(
+                                            onPressed: () {
+                                              DatabaseServices(uid: user.uid)
+                                                  .deleteItem(
+                                                      itemName: name,
+                                                      itemPrice: price,
+                                                      itemQty: qty);
+                                              Navigator.pop(context);
+                                              Navigator.pop(context);
+                                            },
+                                            child: Text("Yes")),
+                                        FlatButton(
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                            child: Text("No"))
+                                      ],
+                                    ));
+                          },
+                          icon: Icon(Icons.delete),
+                          label: Text("Delete"))
+                    ]),
+              ],
+            ),
           ),
         ),
-      ),
-    );
+        bottomSheet: BottomAppBar(
+          elevation: 10,
+          child: Container(
+            // color: Colors.black12,
+            height: MediaQuery.of(context).size.height * 0.07,
+            child: Center(
+                child: IconButton(
+              onPressed: () => Navigator.pop(context),
+              icon: Icon(
+                Icons.close,
+                size: 30,
+              ),
+            )),
+          ),
+        ));
   }
 }
